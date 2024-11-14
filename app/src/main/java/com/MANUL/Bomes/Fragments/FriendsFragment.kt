32d0124bcf0a568fed2b38f0fd4f8ffbb96d53d7
DemonBoss1 <121947683+DemonBoss1@users.noreply.github.com>
2024-11-14 11:52:05 +0300

    add FragmentFriendsBinding

--- app/src/main/java/com/MANUL/Bomes/Fragments/FriendsFragment.kt
+++ app/src/main/java/com/MANUL/Bomes/Fragments/FriendsFragment.kt
@@ -1,7 +1,27 @@
 package com.MANUL.Bomes.Fragments
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.MANUL.Bomes.R
+import com.MANUL.Bomes.databinding.FragmentFriendsBinding
 
 class FriendsFragment : Fragment(R.layout.fragment_friends) {
+
+    private lateinit var binding: FragmentFriendsBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentFriendsBinding.inflate(inflater)
+        return binding.root
+    }
 }
