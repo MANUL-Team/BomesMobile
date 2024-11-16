30de2f8530eaed6700f2a23e60a57bdcbb912aea
DemonBoss1 <121947683+DemonBoss1@users.noreply.github.com>
2024-11-16 14:13:39 +0300

    transition to a test server

--- app/src/main/java/com/MANUL/Bomes/Utils/serverUtils.kt
+++ app/src/main/java/com/MANUL/Bomes/Utils/serverUtils.kt
@@ -3,7 +3,7 @@ package com.MANUL.Bomes.Utils
 import okhttp3.Request
 
 private val BomesRequest = Request.Builder().url("wss://bomes.ru:8000").build()
-private val TestServerRequest = Request.Builder().url("192.168.31.24:8000").build()
+private val TestServerRequest = Request.Builder().url("ws://192.168.31.24:8000").build()
 
 @JvmField
 public val NowRequest = TestServerRequest
